799ec7ad1b7637b2e3d76b6e6ec183e3ed618d2c
Vadim <vsadnik199@gmail.com>
2023-12-12 03:38:12 +0300

    Project created

---
+++ HW41SwiftUI/HW41SwiftUI/HW41SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW41SwiftUIApp.swift
+//  HW41SwiftUI
+//
+//  Created by Вадим Игнатенко on 12.12.23.
+//
+
+import SwiftUI
+
+@main
+struct HW41SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
